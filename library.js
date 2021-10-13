3c465c4b70848012fbbb29718b25c381095b39af
yankeehotelfoxtrot51 <tbgrilliot@gmail.com>
2021-10-13 02:04:27 -0500

    getting fire

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 var noteArray = [];
-var test = "dynamic bunghole";
+var titleGoof = "dynamic bunghole";
 var rowLimitX = 6;
 
 
@@ -14,12 +14,22 @@ function debugKey(){
 var txt2 = $("<i></i>").text("love ");
 function newNote() {
     console.log('new note being made!');
-    //$(".notes-cont").append(`<div class='note'><p>${test}</p></div>`);
-    $(".active-row").append(`<div class='note'><p>${test}</p></div>`);
-    
+
+    //getting text input value
+    var inputTest = document.getElementById("input-for-testing").value;
+
+    //make new note object
+    new Note(titleGoof, inputTest);
+    console.log(noteArray[noteArray.length-1].id);
+
+    $(".active-row").append(`<div class='note'><p>${noteArray[noteArray.length -1].text}</p></div>`);
+    //$(".active-row").append(`<div class='note'><p>${inputTest}</p></div>`);
+
+    //count the notes to see if we need a new row
+    if(noteArray.length % 6 === 0) newRow();
+
 }
 function newRow(){
-    console.log('next row note plas/');
     $(".active-row").removeClass("active-row");
     $(".notes-cont").append(`<div class='row active-row'></div>`);
 }
