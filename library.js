5705d8ed92923595597f17ce184c2a7dbb2cc67c
yankeehotelfoxtrot51 <tbgrilliot@gmail.com>
2021-10-15 01:49:13 -0500

    save button and some responsiveness

--- library.js
+++ library.js
@@ -1,31 +1,26 @@
 var noteArray = [];
-var rowLimitX = 5;
-
+var rowLimitX = Math.floor((window.innerWidth -50) / 280);
+//console.log(screen.width / 400)
 
 function debugKey(){
     $(document).keydown(function (e) {
         if(e.code === "KeyQ"){
-            console.log('debug!');
+            console.log(window.innerWidth);
         }
     });
 }
 
 var txt2 = $("<i></i>").text("love ");
 function newNote() {
-    console.log('new note being made!');
-
-    //getting text input value
-    var inputTest = document.getElementById("input-for-testing").value;
 
     //make new note object
-    new Note(inputTest);
-    console.log(noteArray[noteArray.length-1].id);
+    new Note();
+    console.log(`new note being made! ~ ${noteArray[noteArray.length-1].id}`);
 
-    //var localNoteText = `<p>${noteArray[noteArray.length -1].text}</p>`; <<<old text came from the text input at the top.
-    var localNoteText = `<textarea class='note-textarea' cols='20' rows='12' maxlength='240' placeholder='Write Something!'></textarea>`
     var localEdit = `<button><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>`
     var localTrash = `<button><i class="fa fa-trash-o" aria-hidden="true"></i></button>`
     var localTopBar = `<div class='top-bar'>${localEdit}${localTrash}</div>`
+    var localNoteText = `<textarea class='note-textarea' cols='20' rows='12' maxlength='240' placeholder='Write Something!'></textarea>`
     var localNote = `<div class='note'>${localTopBar}<div class='inner-note'>${localNoteText}</div></div>`;
 
     $(".active-row").append(localNote);
@@ -37,5 +32,24 @@ function newNote() {
 function newRow(){
     $(".active-row").removeClass("active-row");
     $(".notes-cont").append(`<div class='row active-row'></div>`);
+}
+
+function saveAll(){
+    console.log('saving...');
+    //he needs to be able to find each of the note elements and save their data to their objects. Then later he can push that to the DB.
+    //we need to find them all in order. There should be a jquery way to do this. probbalbly with a for loop.
+    //notes-cont
+
+    for (let i = 0; i < noteArray.length; i++) {
+        noteArray[i].text = $('.notes-cont').find('.note-textarea').eq(i).val();
+    }
+
+    /*delete later*/
+    console.log('values saved, reading them back now...');
+    for (let i = 0; i < noteArray.length; i++) {
+        console.log(`note ${i}: ${noteArray[i].text}`);
+    }
+
+
 }
 
